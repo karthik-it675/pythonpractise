0438e6214b6ac4b1748248af3c167eb1d6f90d29
karthik-it675 <boddupallykarthik6@gmail.com>
2026-02-24 23:41:55 +0530

    lambda

---
+++ lamda.py
@@ -0,0 +1,5 @@
+import mymathmodule
+n1=20
+n2=30
+res=mymathmodule.add(n1,n2)
+print(res)
---
+++ slicing.py
@@ -0,0 +1,7 @@
+arr=[1,2,3,4,5]
+i=0
+sumi=0
+while i<len(arr):
+    sumi=sumi+arr[i]
+    i=i+1
+print(sumi)
